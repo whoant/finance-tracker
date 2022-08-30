707a048c67a50db9f878f32d6ada520f19f909d9
whoant <vovanhoangtuan4.2@gmail.com>
2022-08-30 17:22:52 +0700

    Add migration file

---
+++ db/migrate/20220829182853_create_friend_ships.rb
@@ -0,0 +1,10 @@
+class CreateFriendShips < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friend_ships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :friend, references: :users, foreign_key: { to_table: :users }
+      
+      t.timestamps
+    end
+  end
+end
